a48e30c790db71a7e92d798073b886a3d20edf25
CommanderCortex <cjcotton88@gmail.com>
2019-06-19 09:07:37 +1200

    Ok

--- src/main/java/me/cortex/Economy/utils/MongoConnect.java
+++ src/main/java/me/cortex/Economy/utils/MongoConnect.java
@@ -2,11 +2,18 @@ package me.cortex.Economy.utils;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import me.cortex.Economy.CEconomy;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import javax.print.Doc;
+
 
 public class MongoConnect {
     private MongoDatabase database;
+    private MongoCollection playerData;
     private CEconomy plugin = CEconomy.getPlugin();
 
 
@@ -14,6 +21,32 @@ public class MongoConnect {
         MongoClient Client = new MongoClient(new MongoClientURI(plugin.getConfig().getString("uri")));
         setDatabase(Client.getDatabase("CEconomy"));
         MessageManager.ConsoleGood("DataBase Connected");
+    }
+
+    public void setPlayerDataDocument(Object value, String identifier, String uuid){
+        Document document = new Document("uuid", uuid);
+        Bson newValue = new Document(identifier, value);
+        Bson updateOperation = new Document("$set", newValue);
+        playerData.updateOne(document, updateOperation);
+    }
+
+    public Object getPlayerDataDocument(String identifier, String uuid){
+        Document document = new Document("uuid", uuid);
+        if(playerData.find(document).first() != null){
+            Document found = (Document) playerData.find().first();
+            return found.get(identifier);
+        }
+
+        MessageManager.ConsoleBad("Document in Null for UUID: " +  uuid);
+        return null;
+    }
+
+    public MongoCollection getPlayerData() {
+        return playerData;
+    }
+
+    public void setPlayerData(MongoCollection playerData) {
+        playerData = playerData;
     }
 
     public MongoDatabase getDatabase(){
